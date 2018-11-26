baf4b7f7ca85569b0cafbc8096f2c5346b1410a8
shion-sagawa <shion.s0618@gmail.com>
2018-11-26 11:18:27 +0900

    [add] cameraの基底クラスの追加
     -  このクラスの派生クラスをタイトルシーンやゲームシーンそれぞれで使うため

---
+++ Lonely/Lonely/Game/Scene/Camera/Camera.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+	/**‘€ìŠÖ”*/
+	void Update();
+
+private:
+
+};
+
+Camera::Camera()
+{
+}
+
+Camera::~Camera()
+{
+}
+
+void Camera::Update()
+{
+
+}

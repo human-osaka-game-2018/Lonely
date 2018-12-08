f32eb1dd92175c005e4d4b92264e1d66831b850c
shion-sagawa <shion.s0618@gmail.com>
2018-12-08 14:44:46 +0900

    [add] ObjectBaseから派生したObject２DBaseとObject３DBaseを追加

---
+++ Lonely/Lonely/GameLib/ObjectManager/Object2DBase.cpp
@@ -0,0 +1,34 @@
+/**
+* @file Object2DBase.cpp
+* @brief Object2DBaseクラスのソースファイル
+* @author 佐川
+*/
+
+#include "Object2DBase.h"
+
+
+Object2DBase::Object2DBase()
+{
+}
+
+Object2DBase::~Object2DBase()
+{
+}
+
+bool Object2DBase::Initialize()
+{
+	return true;
+}
+
+void Object2DBase::Finalize()
+{
+}
+
+void Object2DBase::Update()
+{
+}
+
+void Object2DBase::Render()
+{
+}
+
---
+++ Lonely/Lonely/GameLib/ObjectManager/Object2DBase.h
@@ -0,0 +1,48 @@
+/**
+* @file Object2DBase.h
+* @brief Object2DBaseクラスのヘッダファイル
+* @author 佐川
+*/
+
+#pragma once
+
+#include "Object.h"
+#include "../2DHelper/2DHelper.h"
+#include "../FBX/Texture.h"
+
+/**
+* @brief ２Dオブジェクトの基底となるクラス
+*/
+class Object2DBase: public Object
+{
+public:
+
+	Object2DBase();
+	virtual ~Object2DBase();
+
+	/**
+	* @brief 初期化を行う関数
+	*/
+	virtual bool Initialize();
+
+	/**
+	* @brief 解放を行う関数
+	*/
+	virtual void Finalize();
+
+	/**
+	* @brief 操作の更新を行う関数
+	*/
+	virtual void Update();
+
+	/**
+	* @brief 描画を行う関数
+	*/
+	virtual void Render();
+
+protected:
+
+	Texture         m_texture;
+	Simple2DVertex  m_vertices[4]; //!< 矩形の4頂点
+	bool canRender = true;         //!<　描画できるかどうか
+};

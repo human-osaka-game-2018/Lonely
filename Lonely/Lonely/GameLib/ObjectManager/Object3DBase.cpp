8afdcd76ba1d4be7cbae01f9e5149ed6a0b7e2d8
shion-sagawa <shion.s0618@gmail.com>
2018-12-08 14:44:46 +0900

    [add] ObjectBaseから派生したObject２DBaseとObject３DBaseを追加

---
+++ Lonely/Lonely/GameLib/ObjectManager/Object3DBase.cpp
@@ -0,0 +1,34 @@
+/**
+* @file Object3DBase.cpp
+* @brief Object3DBaseクラスのソースファイル
+* @author 佐川
+*/
+
+#include "Object3DBase.h"
+
+
+Object3DBase::Object3DBase()
+{
+}
+
+Object3DBase::~Object3DBase()
+{
+}
+
+bool Object3DBase::Initialize()
+{
+	return true;
+}
+
+void Object3DBase::Finalize()
+{
+}
+
+void Object3DBase::Update()
+{
+}
+
+void Object3DBase::Render()
+{
+}
+
---
+++ Lonely/Lonely/GameLib/ObjectManager/Object3DBase.h
@@ -0,0 +1,44 @@
+/**
+* @file Object3DBase.h
+* @brief Object3DBaseクラスのヘッダファイル
+* @author 佐川
+*/
+
+#pragma once
+
+#include "Object.h"
+
+/**
+* @brief 3Dオブジェクトの基底となるクラス
+*/
+class Object3DBase : public Object
+{
+public:
+
+	Object3DBase();
+	virtual ~Object3DBase();
+
+	/**
+	* @brief 初期化を行う関数
+	*/
+	virtual bool Initialize();
+
+	/**
+	* @brief 解放を行う関数
+	*/
+	virtual void Finalize();
+
+	/**
+	* @brief 操作の更新を行う関数
+	*/
+	virtual void Update();
+
+	/**
+	* @brief 描画を行う関数
+	*/
+	virtual void Render();
+
+protected:
+
+	bool canRender = true;         //!<　描画できるかどうか
+};

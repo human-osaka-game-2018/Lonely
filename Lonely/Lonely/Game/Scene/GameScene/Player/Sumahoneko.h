28d2e6d16656a38ed3433b797908445ab41ab5a1
shion-sagawa <shion.s0618@gmail.com>
2019-02-02 18:16:35 +0900

    [issue #23] ゲームオーバー処理の実装

---
+++ Lonely/Lonely/Game/Scene/GameScene/Player/Sumahoneko.h
@@ -0,0 +1,121 @@
+﻿
+/**
+* @file Sumahoneko.h
+* @brief Sumahonekoクラスのヘッダファイル
+* @author shion-sagawa
+*/
+
+#pragma once
+
+#include "PlayerBase.h"
+#include "../../../../GameLib/FBX/FBX.h"
+#include "../SharedInformation/SharedInformation.h"
+
+enum DYING_ANIME_STATE
+{
+	ONE,
+	TWO,
+	THREE,
+	FOUR
+};
+
+/**
+* @brief プレイヤー関係の処理をまとめたクラス
+*/
+class Sumahoneko : public PlayerBase
+{
+public:
+
+	Sumahoneko();
+	~Sumahoneko();
+
+	/**
+	* @brief 初期化関数
+	*/
+	bool Initialize();
+	/**
+	* @brief 解放関数
+	*/
+	void Finalize();
+
+	/**
+	* @brief 更新を行う関数
+	*/
+	void Update();
+
+	/**
+	* @brief 描画を行う関数
+	*/
+	void Render();
+
+	/**
+	* @brief フレームで処理した移動量を、衝突していたので戻す関数
+	*/
+	void RevertPosition();
+
+	/**
+	* @brief フレームで処理したY軸移動量を、衝突していたので戻す関数
+	*/
+	void RevertPositionY();
+
+	/**
+	* @brief スマホ猫が落下している関数
+	*/
+	void Fall();
+
+	/**
+	* @brief ジャンプを行う関数
+	*/
+	void Jump();
+
+	/**
+	* @brief ダメージを受けている関数
+	*/
+	void Damaged();
+
+	/**
+	* @brief ダメージ受けて無敵時間を数える関数
+	*/
+	void UpdateDamageCount();
+
+	void AnimateDead();
+
+	void SetIsLanded(bool isLanded) { m_isLanded = isLanded; };
+
+private:
+
+	void DetectUpCollidingWithStage(D3DXVECTOR3* movementThisFrame);
+
+	void DetectDownCollidingWithStage(D3DXVECTOR3* movementThisFrame);
+
+	void DetectXCollidingWithStage(D3DXVECTOR3* movementThisFrame);
+
+	void DetectZCollidingWithStage(D3DXVECTOR3* movementThisFrame);
+
+	FbxModel	m_fbxModel;
+	Model		m_shpere;
+	float		m_radius = 0.1f;
+	float       m_playerDirection = 0.f;
+	float       m_gravity = 0.f;
+	float       m_maxGravity = -5.f;
+	float       m_jumpingTime = 0.f;
+	//float       m_accelerationGravity = 0.0025f;
+	//float       m_initialVelocity = 0.5f;
+
+	D3DXVECTOR3 m_rotation = { 0.f,0.f,0.f };
+	float       m_accelerationGravity = 0.0001f;
+	float       m_initialVelocity = 1.f;
+	bool        m_isLanded = false;
+	bool        m_runs = false;
+	bool        m_damaged = false;
+	bool        m_dead = false;
+	int         m_hp = 3;
+	int         m_damageCount;
+	int         m_dyingAnimeState;
+	int         m_dyingAnimeCount;
+	float       m_radian = 0.f;
+
+	SharedInformation* m_pSharedInformation;
+
+	FbxModel*   m_pFbxStage;//今はとりあえずこのクラスに置いているだけ
+};

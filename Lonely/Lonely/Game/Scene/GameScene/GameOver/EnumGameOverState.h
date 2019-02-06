f7feb122a1edb95aeab4a4fb0d0b32fb27dca71f
shion-sagawa <shion.s0618@gmail.com>
2019-02-06 18:03:46 +0900

    [issue #4] FBXファイルの追加

---
+++ Lonely/Lonely/Game/Scene/GameScene/GameOver/EnumGameOverState.h
@@ -0,0 +1,16 @@
+/**
+* @file EnumGameOverState.h
+* @brief GameOverStateのEnumを定義するファイル
+* @author shion-sagawa
+*/
+#pragma once
+
+
+/**
+* @brief GameOverがどんな状態か
+*/
+enum GAME_OVER_STATE
+{
+	CONTINUE,
+	BACK_TO_TITLE
+};

a9a1d8024203d6eaf365517d6b72e3406a5ab84b
shion-sagawa <shion.s0618@gmail.com>
2019-03-26 20:37:31 +0900

    [issue #29] シーン遷移の処理の修正
     -  シーンマネージャーの遷移関数の仕様変更、メンバ変数の追加

---
+++ Lonely/Lonely/GameLib/SceneManager/Enum_Scene.h
@@ -0,0 +1,14 @@
+﻿/**
+* @file   Enum_Scene.h
+* @brief  シーンの種類のEnumを定義しているヘッダファイル
+* @author shion-sagawa
+*/
+
+#pragma once
+
+enum SCENE
+{
+		TITLE_SCENE,
+		DATA_SELECT_SCENE,
+		GAME_SCENE
+};
--- Lonely/Lonely/GameLib/SceneManager/SceneManager.cpp
+++ Lonely/Lonely/GameLib/SceneManager/SceneManager.cpp
@@ -4,61 +4,106 @@
 * @author shion-sagawa
 */
 
-#include "../GameLib.h"
 #include "SceneManager.h"
+
+#include "GameLib.h"
+
+#include "Enum_Scene.h"
+#include "../../Game/Scene/TitleScene/TitleScene.h"
+#include "../../Game/Scene/DataSelectScene/DataSelectScene.h"
+#include "../../Game/Scene/GameScene/GameScene.h"
+
 
 SceneManager::SceneManager()
 	: m_pScene(nullptr)
-{}
+	, m_currentScene(TITLE_SCENE)
+	, m_nextScene(TITLE_SCENE)
+{
+}
 
 SceneManager::~SceneManager()
 {
 	Finalize();
 }
 
-/**シーン管理の初期化*/
+//GameLibの初期化関数で一度だけSceneManagerの初期化関数が呼ばれる
 void SceneManager::Initialize()
 {
+	CreateScene();
+}
+
+void SceneManager::Finalize()
+{
+	ReleaseScene();
+}
+
+void SceneManager::Update()
+{
+	//シーン遷移すべきなら遷移関数を呼ぶ
+	if (m_currentScene != m_nextScene)
+	{
+		SwitchScene();
+	}
+
+	//現在のシーンの処理を更新する
+	if (m_pScene)
+	{
+		m_pScene->Update();
+	}
+}
+
+void SceneManager::Render()
+{
+	if (m_pScene)
+	{
+		m_pScene->Render();
+	}
+}
+
+void SceneManager::SwitchScene()
+{
+	//現在のシーンを解放する
+	ReleaseScene();
+
+	//次のシーンを作成する
+	CreateScene();
+}
+
+void SceneManager::ReleaseScene()
+{
+	m_pScene->Finalize();
+	delete m_pScene;
 	m_pScene = nullptr;
 }
 
-/**シーン管理の解放*/
-void SceneManager::Finalize()
+void SceneManager::CreateScene()
 {
-	// 最後は解放して終わる
-	if (m_pScene) { m_pScene->Finalize(); }
-	SAFE_DELETE(m_pScene);
-}
-
-/**シーンの更新*/
-void SceneManager::Update()
-{
-	if (m_pScene) m_pScene->Update();
-}
-
-/**シーンの描画*/
-void SceneManager::Render()
-{
-	if (m_pScene) m_pScene->Render();
-
-}
-
-/**シーンの遷移*/
-bool SceneManager::ChangeScene(SceneBase* pScene)
-{
-	// 現在のシーンを解放する
-	if (m_pScene)
+	switch (m_nextScene)
 	{
-		m_pScene->Finalize();
-		SAFE_DELETE(m_pScene);
+	case TITLE_SCENE:
+
+		m_pScene = new TitleScene();
+		break;
+
+	case DATA_SELECT_SCENE:
+
+		m_pScene = new DataSelectScene();
+		break;
+
+	case GAME_SCENE:
+
+		m_pScene = new GameScene();
+		break;
+
+	default:
+
+		break;
 	}
 
-	bool ret = true;
-	// 次のシーンがあれば初期化する
-	if (pScene)
-	{
-		ret = pScene->Initialize();
-	}
-	m_pScene = pScene;
-	return ret;
+	m_pScene->Initialize();
+}
+
+void SceneManager::SwitchSceneNextFrame(int nextScene)
+{
+	m_nextScene = nextScene;
 }
